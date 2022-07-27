98b002183c33b09f319ecbe5007ddc114e1b5ce3
Gustavo Oliveira <gustavo.santosoliveira@outlook.com.br>
2022-07-27 08:26:47 -0300

    colocando o codigo no site

---
+++ nivel12exercicio1/nivel12exercicio1.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Idade do animal:", Idade(10))
+}
+
+func Idade(i int) int {
+	return i * 7
+}
+
+//  - Crie uma função Idade, que toma como parâmetro um número de anos e retorna a idade
+// equivalente em anos caninos. (1 ano humano → 7 anos caninos)
+// - Documente seu código com comentários, e utilize a função Idade na sua função main.
+// - Rode seu programa para verificar se ele funciona.
+// - Rode um local server com godoc e leia sua documentação.
